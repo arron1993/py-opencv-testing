39a29de3fc261aef4e369728d4904813d2d8a0a6
Arron Moore <arron.moore93@gmail.com>
2022-06-24 17:07:48 +0100

    Added script to extract images from an image

---
+++ image-extraction/main.py
@@ -0,0 +1,58 @@
+import numpy as np
+import cv2 as cv
+
+
+def load_image():
+    return cv.imread("./in.jpg")
+
+
+def grayscale(img):
+    return cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+
+def gaussian_blur(img):
+    return cv.GaussianBlur(img, (5, 5), 0)
+
+
+def otsu_binarization(img):
+    ret3, img = cv.threshold(img, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+    return img
+
+
+def save(img):
+    cv.imwrite("./out.jpg", img)
+
+
+def dilate(img):
+    iterations = 1
+    kernel = np.ones((5, 5), np.uint8)
+    return cv.dilate(img, kernel, iterations)
+
+
+def find_contours(img):
+    return cv.findContours(img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+
+def find_bounding_boxes(contours):
+    return [cv.boundingRect(c) for c in contours]
+
+
+def main():
+    original_img = load_image()
+
+    img = grayscale(original_img)
+    img = gaussian_blur(img)
+    img = otsu_binarization(img)
+    img = dilate(img)
+    contours, hierarchy = find_contours(img)
+    boxes = find_bounding_boxes(contours)
+    i = 0
+    for x, y, w, h in boxes:
+        cropped = original_img[y : y + h, x : x + w]
+        cv.imwrite(f"./results/ROI_{i}.jpg", cropped)
+        i += 1
+    save(img)
+
+
+if __name__ == "__main__":
+    main()
